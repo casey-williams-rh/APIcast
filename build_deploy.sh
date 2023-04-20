ddd91fc93c37c8f2a1cba5c547f781c2a66821a7
Casey Williams <caswilli@redhat.com>
2023-04-20 15:58:45 -0400

    Update Build Job for the ApiCast-Base Image

--- build_deploy.sh
+++ build_deploy.sh
@@ -15,3 +15,8 @@ mkdir -p "$DOCKER_CONF"
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 docker --config="$DOCKER_CONF" build --no-cache -t "${IMAGE}:${IMAGE_TAG}" .
 docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
+
+if [[ $GIT_BRANCH == *"security-compliance"* ]]; then
+    docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:apicast-base-security-compliance"
+    docker --config="$DOCKER_CONF" push "${IMAGE}:apicast-base-security-compliance"
+fi
